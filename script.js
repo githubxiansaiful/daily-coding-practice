64df59d0b238aeb10849625f09c40a23da37a1eb
Xian Saiful <xiansaiful@gmail.com>
2026-02-15 14:41:05 +0600

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Hello, World!");
+const numbers = [1, 2, 3, 4, 5];
+
+// Using forEach to log each number
+numbers.forEach(function (number) {
+    console.log(number);
 });
