a1ba7c9d6567358ab3b1f147a474c84b9e22d208
Xian Saiful <xiansaiful@gmail.com>
2026-02-16 21:31:44 +0600

    Regular practice

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const numbers = [1, 2, 3, 4, 5];
 
 // Using forEach to log each number
 numbers.forEach(function (number) {
+    // console.log(number);
+});
+
+numbers.push(7); // Adding a new number to the array
+// Using forEach again to log each number after adding a new one
+numbers.forEach(function (number) {
     console.log(number);
 });
+
+Test
