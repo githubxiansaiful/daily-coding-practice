e89bb6a6f1649edfb1d3ca0fe7063706b3e1ee8e
Xian Saiful <xiansaiful@gmail.com>
2026-02-17 17:26:13 +0600

    Add loadTodosData function and update Todo List UI with buttons

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Script file connected!!");
+
+const loadTodosData = () => {
+    fetch("https://dummyjson.com/todos")
+        .then(res => res.json())
+        .then(data => console.log(data)
+        )
+}
