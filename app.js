29ca8a7f835bdbbc67c77341db16fb84968e8ecd
Xian Saiful <xiansaiful@gmail.com>
2026-02-17 18:19:20 +0600

    Add rendering of todos and update HTML structure with async button

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-console.log("Script file connected!!");
-
+// JSON API URL
 const todosAPIURL = "https://dummyjson.com/todos";
 
+// Render todos to the DOM
+const renderTodos = (todos) => {
+    const container = document.getElementById("todos");
+    container.innerHTML = "";
+
+    todos.forEach((todo) => {
+        const div = document.createElement("div");
+        div.className = "todo-item";
+        div.innerHTML = `
+            <span>${todo.todo}</span>
+            <span>${todo.completed}</span>
+        `;
+
+        container.appendChild(div);
+    })
+
+}
+
+// Fetch todos using .then() method
 const loadTodosThen = () => {
     fetch(todosAPIURL)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log("Data From .then:", data.todos)
+            renderTodos(data.todos)
+        })
 }
 
+// Fetch todos using async/await
 const loadTodosAsync = async () => {
     const res = await fetch(todosAPIURL);
     const data = await res.json();
-    console.log(data);
+    console.log("Data From async:", data);
 }
