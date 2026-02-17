312d6b43792e1a802472620a38b9ae55bedadd98
Xian Saiful <xiansaiful@gmail.com>
2026-02-17 18:46:11 +0600

    Enhance Todo rendering with dynamic styles and add Font Awesome icons; create todo-style.css for layout

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const renderTodos = (todos) => {
 
     todos.forEach((todo) => {
         const div = document.createElement("div");
-        div.className = "todo-item";
+        div.className = `single-item ${todo.completed ? "bg-[#778beb]" : "bg-[#e15f413b]"}`;
         div.innerHTML = `
             <span>${todo.todo}</span>
-            <span>${todo.completed}</span>
+            <span><i class="${todo.completed ? 'fa-solid fa-circle-check' : 'fa-regular fa-circle-xmark'}"></i></span>
         `;
 
         container.appendChild(div);
@@ -25,7 +25,6 @@ const loadTodosThen = () => {
         .then(res => res.json())
         .then(data => {
             console.log("Data From .then:", data.todos)
-            renderTodos(data.todos)
         })
 }
 
@@ -34,4 +33,5 @@ const loadTodosAsync = async () => {
     const res = await fetch(todosAPIURL);
     const data = await res.json();
     console.log("Data From async:", data);
+    renderTodos(data.todos);
 }
