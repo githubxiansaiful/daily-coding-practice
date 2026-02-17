4bb35e7d8354d5595616eefacbf447f44bbc13f9
Xian Saiful <xiansaiful@gmail.com>
2026-02-17 17:36:33 +0600

    Refactor todo loading functions and update button event handlers in HTML

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 console.log("Script file connected!!");
 
-const loadTodosData = () => {
-    fetch("https://dummyjson.com/todos")
+const todosAPIURL = "https://dummyjson.com/todos";
+
+const loadTodosThen = () => {
+    fetch(todosAPIURL)
         .then(res => res.json())
-        .then(data => console.log(data)
-        )
+        .then(data => console.log(data))
+}
+
+const loadTodosAsync = async () => {
+    const res = await fetch(todosAPIURL);
+    const data = await res.json();
+    console.log(data);
 }
